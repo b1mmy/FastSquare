8f4ed9eb988f1d3af61fe1d9c79c1c3e42be956b
b1mmy <bimmy25@mail.ru>
2018-07-01 01:48:52 +0500

    connection check

--- js/script.js
+++ js/script.js
@@ -2,12 +2,18 @@
 var settings = {
   url: 'https://source.unsplash.com/random?',
   sections: ['Cats','Dogs','Food','World'],
-  current: 'Cats'
+  current: 'Cats',
+  slideshow: 'false',
+  timing: 10,
+  duration: 1000,
+  hide: 2500
 };
 /* End settings for app */
 /* Local storage settings */
 if(!localStorage.getItem("sections")) localStorage.setItem("sections",JSON.stringify(settings.sections));
 if(!localStorage.getItem("current")) localStorage.setItem("current",settings.current);
+if(!localStorage.getItem("timing")) localStorage.setItem("timing",settings.timing);
+if(!localStorage.getItem("slideshow")) localStorage.setItem("slideshow",settings.slideshow);
 var url = settings.url,
     sections = JSON.parse(localStorage.getItem("sections")),
     current = localStorage.getItem("current"),
@@ -15,19 +21,82 @@ var url = settings.url,
     block = false,
     options = '',
     inputs = '',
-    images = document.querySelectorAll('#all .img');
+    images = document.querySelectorAll('#all .img'),
+    slideshow = localStorage.getItem("slideshow"),
+    timing = localStorage.getItem("timing"),
+    mouse = 0;
 /* End local storage settings */
-/* Call functions */
-refreshSections();
-modalPos();
-window.onresize = function(){modalPos();};
-for(var i = 0; i < images.length; i++) document.querySelectorAll('#all .img')[i].onclick = function(){changeImg();};
-document.getElementById('refresh').onclick = function(){changeImg();};
-document.getElementById('settings').onclick = function(){modal();};
-document.getElementById('modal').addEventListener('click', function (e) {if(e.target === e.currentTarget) modal();});
-changeImg();
+/* Start app */
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+  checkInternet();
+};
 /* End of calls */
 /* Functions */
+function checkInternet() {
+  var img = document.body.appendChild(document.createElement("img"));
+  img.classList.add('connect');
+  img.src = settings.url;
+  img.onload = function(){
+    var elems = document.querySelectorAll('.connect');
+    for(var i = 0; i < elems.length; i++) elems[i].remove();
+    document.getElementById('notconnected').style.display = 'none';
+    start();
+  };
+  img.onerror = function(){
+    var elems = document.querySelectorAll('.connect');
+    for(var i = 0; i < elems.length; i++) elems[i].remove();
+    document.getElementById('notconnected').style.display = 'block';
+    setTimeout(function(){checkInternet();},1000);
+  };
+}
+function start() {
+  refreshSections();
+  modalPos();
+  window.onresize = function(){modalPos();};
+  for(var i = 0; i < images.length; i++) document.querySelectorAll('#all .img')[i].onclick = function(){
+    if(mouse*100 <= settings.hide) {
+      changeImg();
+    } else {
+      showelems();
+    }
+  };
+  document.addEventListener('mousemove', showelems);
+  document.getElementById('settings').onclick = function(){modal();};
+  document.getElementById('modal').addEventListener('click', function (e) {if(e.target === e.currentTarget) modal();});
+  document.getElementById('timing').value = timing;
+  document.getElementById('timing').onkeyup = function() {
+    timing = parseInt(this.value,10);
+    localStorage.setItem("timing",timing);
+  };
+  document.getElementById('slideshowpower').value = slideshow;
+  document.getElementById('slideshowpower').onchange = function(){
+    slideshow = this.value == 'true' ? 'true' : 'false';
+    localStorage.setItem("slideshow",slideshow);
+    modal();
+  };
+  changeImg();
+  slideshowscroll(false);
+  mousenomove();
+}
+function showelems() {
+  mouse = 0;
+  document.getElementById('settings').classList.remove('hide');
+  document.getElementById('section').classList.remove('hide');
+}
+function mousenomove() {
+  if(mouse*100 <= settings.hide) {
+    mouse++;
+  } else {
+    document.getElementById('settings').classList.add('hide');
+    document.getElementById('section').classList.add('hide');
+  }
+  setTimeout(function(){mousenomove();},100);
+}
+function slideshowscroll(go) {
+  if(slideshow == 'true' && go) changeImg();
+  setTimeout(function(){slideshowscroll(true);},timing*settings.duration);
+}
 function changeImg() {
   var item = document.getElementsByClassName('img')[cur];
   item.style.backgroundImage = 'url(\'' + url + current + '&' + new Date().getTime() + '\')';
@@ -45,14 +114,17 @@ function changeSection(elem) {
 }
 function modalPos() {
   var elem = document.getElementById('modal-body');
-  var elems = document.querySelectorAll('#modal-body *');
-  var summ = 0;
-  document.querySelector('*').style.fontSize = "2.5v" + (window.screen.availWidth > window.screen.availHeight ? "w" : "h");
+  document.querySelector('*').style.fontSize = "2.5v" + (window.screen.availWidth > window.screen.availHeight ? "w" : "h") + "!important";
+  document.querySelector('#modal-body h1').style.fontSize = "2.5v" + (window.screen.availWidth > window.screen.availHeight ? "w" : "h") + "!important";
+  document.querySelector('#modal-body h2').style.fontSize = "1.5v" + (window.screen.availWidth > window.screen.availHeight ? "w" : "h") + "!important";
   elem.style.maxWidth = window.screen.availWidth <= 480 ? "100vw" : "50vw";
-  elem.style.maxHeight = window.screen.availHeight <= 480 ? "100vh" : "50vh";
-  elem.style.marginTop = "calc((100vh - " + elem.offsetHeight + "px)/2)";
-  for(var i = 0; i < elems.length; i++) summ += elems[i].offsetHeight;
-  elem.style.paddingTop = "calc((" + elem.offsetHeight + "px - " + summ + "px)/2)";
+  if(elem.offsetHeight < document.getElementById('modal').offsetHeight+50) {
+    elem.style.marginTop = "calc((100vh - " + elem.offsetHeight + "px)/2)";
+    elem.style.marginBottom = "0";
+  } else {
+    elem.style.marginTop = "50px";
+    elem.style.marginBottom = "50px";
+  }
 }
 function refreshSections() {
   options = '';
@@ -66,15 +138,17 @@ function refreshSections() {
   inputs += '<button id="plus"><i class="icon icon-add"></i></button>';
   inputs += '<button id="refreshSections"><i class="icon icon-ok"></i></button>';
   inputs += '<button id="reset"><i class="icon icon-reset"></i></button>';
+  inputs += '<button id="exit"><i class="icon icon-off"></i></button>';
   inputs += '</div>';
   document.getElementById('section').innerHTML = options;
-  document.getElementById('modal-body').innerHTML = inputs;
+  document.getElementById('sections').innerHTML = inputs;
   current = localStorage.getItem("current");
   document.getElementById('section').value = (sections.indexOf(current)+1 ? current : sections[0]);
   document.getElementById('section').onchange = function(){changeSection(this);};
   document.getElementById('refreshSections').onclick = function(){addSections(true);};
   document.getElementById('reset').onclick = function(){reset();};
   document.getElementById('plus').onclick = function(){addSection();};
+  document.getElementById('exit').onclick = function(){navigator.app.exitApp();};
 }
 function addSection() {
   addSections(false);
@@ -91,7 +165,7 @@ function reset() {
   changeImg();
 }
 function addSections(ok) {
-  var items = document.querySelectorAll('#modal-body input'),arr = [],re = /^[a-zA-ZА-Яа-яЁё\\s]+$/;
+  var items = document.querySelectorAll('#sections input'),arr = [],re = /^[a-zA-ZА-Яа-яЁё\\s]+$/;
   for(var i = 0; i < items.length; i++) if(items[i].value.trim().length > 2 && re.test(items[i].value.trim())) arr.push(items[i].value);
   if(arr.length > 0) {
     sections = arr;
